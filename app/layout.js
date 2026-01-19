68dd0b97fc58e601988c099dd7b3e500597755b9
Sourav <sourav@Souravs-MacBook-Air.local>
2026-01-19 16:40:17 +0530

    after titile

--- app/layout.js
+++ app/layout.js
@@ -10,7 +10,7 @@ const poppins = Poppins({
 });
 
 export const metadata = {
-  title: "Shameer Cars",
+  title: "Lyf Ads ",
   description: " We are the best when it comes to Exotic Cars.",
   icons: {
     icon: "/brandLogo.png", 
