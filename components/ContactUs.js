ba78569ad26de627356e24de82f158c7954f05bc
SouravO <o.sourav5@gmail.com>
2026-02-18 20:14:09 +0530

    bnglr

--- components/ContactUs.js
+++ components/ContactUs.js
@@ -114,12 +114,7 @@ export default function TacticalBentoTerminal() {
 
             {/* THE IFRAME MAP */}
             <div className="flex-1 relative">
-              <iframe
-                title="map"
-                className="w-full h-[450px] grayscale contrast-125"
-                src="https://www.google.com/maps?q=Ernakulam%20Kerala&output=embed"
-                loading="lazy"
-              />
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d248849.90089833894!2d77.46612767685893!3d12.953945615107429!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae1670c9b44e6d%3A0xf8dfc3e8517e4fe0!2sBengaluru%2C%20Karnataka!5e0!3m2!1sen!2sin!4v1771422734732!5m2!1sen!2sin" width="600" height="450" style={{border:0}} allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
               
               {/* RADAR SWEEP EFFECT */}
               <motion.div 
